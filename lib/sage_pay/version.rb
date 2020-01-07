6751de33b7d8399b35ac2a474f94d65d1bd5016f
Peter Vandenberk <peter.vandenberk@simplybusiness.co.uk>
2020-01-07 15:07:24 +0000

    gemstash versioning for dependabot

--- lib/sage_pay/version.rb
+++ lib/sage_pay/version.rb
@@ -1,3 +1,7 @@
 module SagePay
-  VERSION = '1.0.2'
+  VERSION = '1.0.2' # rubocop:disable Style/MutableConstant
+
+  # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
+  VERSION << '.' << ENV['GEM_PRE_RELEASE'].strip \
+  unless ENV.fetch('GEM_PRE_RELEASE', '').strip.empty?
 end
